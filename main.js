327611640b2c32da60b76f0338e6ed598ac8e843
Nexten_ <christianprenholato10@gmail.com>
2022-06-12 14:19:11 -0300

    restringir acesso

--- main.js
+++ main.js
@@ -3,6 +3,19 @@
 // }
 // function test(){
 // } 
+let params = new URLSearchParams(window.location.search.substr(1))
+let show = params.get("a")
+head = document.getElementsByTagName('head')[0]
+
+
+if(show =="1"){
+  head.removeChild(head.children[4])
+
+  const link = document.createElement('link')
+  link.rel ="stylesheet"
+  link.href ="./style.css"
+  head.appendChild(link)
+}
 
 const video = document.getElementById('video')
 const observer = new IntersectionObserver(
